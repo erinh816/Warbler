cf7e435e0d735f796f28fb83b3c69c409f3dc2bb
Silu Hu <erinh816@gmail.com>
2023-10-12 21:08:57 -0400

    add 3 tests in test_user_model.py

--- test_user_model.py
+++ test_user_model.py
@@ -9,6 +9,7 @@ import os
 from unittest import TestCase
 
 from models import db, User, Message, Follow
+from sqlalchemy.exc import IntegrityError
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -49,3 +50,62 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+        self.assertEqual(len(u1.liked_messages),0)
+
+    def test_is_following(self):
+        '''test u1 is  following u2'''
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        db.session.commit()
+
+        self.assertFalse(u1.is_following(u2))
+
+        u1.following.append(u2)
+
+        self.assertTrue(u1.is_following(u2))
+
+        self.assertEqual(len(u1.following),1)
+        self.assertEqual(len(u2.followers),1)
+
+    def test_is_followed_by(self):
+        '''test u1 is followed by u2'''
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        db.session.commit()
+
+        self.assertFalse(u1.is_followed_by(u2))
+
+        u2.following.append(u1)
+
+        self.assertTrue(u1.is_followed_by(u2))
+
+        self.assertEqual(len(u1.followers),1)
+        self.assertEqual(len(u2.following),1)
+
+
+    def test_user_signup(self):
+        '''test signup class method success'''
+
+        test_user = User.signup('testname', 'test@test.com', 'password', None)
+
+        db.session.add(test_user)
+        db.session.commit()
+
+        self.assertEqual(test_user.username, 'testname')
+        self.assertEqual(test_user.email, 'test@test.com')
+        self.assertEqual(User.query.count(), 3)
+
+
+    def test_user_signup(self):
+        '''test signup class method fail'''
+
+        test_user = User.signup('u1', 'test@test.com', 'password', None)
+
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
+
+        db.session.rollback()
+        self.assertEqual(User.query.count(), 2)
